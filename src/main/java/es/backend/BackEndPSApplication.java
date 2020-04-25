cc676b496c40892cfea591d4bd39079f09b91fea
patseho <juanv4004@gmail.com>
2020-04-25 18:31:16 +0200

    Corregidos repositorios de Artista y Cancion (sin probar)

--- src/main/java/es/backend/BackEndPSApplication.java
+++ src/main/java/es/backend/BackEndPSApplication.java
@@ -1,11 +1,11 @@
 package es.backend;
 
+import es.backend.model.Artista;
+import es.backend.services.ArtistaService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackEndPSApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(BackEndPSApplication.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(BackEndPSApplication.class, args); }
 }
